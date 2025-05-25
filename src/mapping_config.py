d44f7a0a66b7c3fa74594ef4f6e154adb1c02468
lilSultano <medi.mido88@gmail.com>
2025-05-25 17:57:36 +0200

    first commit

---
+++ src/mapping_config.py
@@ -0,0 +1,16 @@
+author_to_party_map = {
+    'giorgiameloni_ufficiale': "FRATELLI D'ITALIA CON GIORGIA MELONI",
+    'matteosalviniufficiale': "LEGA PER SALVINI PREMIER",
+    'giuseppeconte_it': "MOVIMENTO 5 STELLE",
+    'movimento5stelle_tiktok': "MOVIMENTO 5 STELLE",  # Account ufficiale del partito (se esiste e così nominato)
+    'partitodemocratico': "PARTITO DEMOCRATICO - ITALIA DEMOCRATICA E PROGRESSISTA",
+    'matteorenziufficiale': "AZIONE - ITALIA VIVA - CALENDA",
+    'forzaitaliaufficiale': "FORZA ITALIA",  # Ipotetico, verificare se esiste e come si chiama l'account
+    'nicolaporrotoctoc': None,  # Giornalista
+    'massimiliano.m5s': "MOVIMENTO 5 STELLE",  # Esponente M5S
+    'luca.carabetta': "MOVIMENTO 5 STELLE",  # Esponente M5S (Luca Carabetta)
+    'gianpaolocataniaitalexit': "ITALEXIT PER L'ITALIA",  # Italexit
+    'danilo.toninelli': "MOVIMENTO 5 STELLE",  # Esponente M5S (Danilo Toninelli)
+    'giuseppe_chiazzese_m5s': "MOVIMENTO 5 STELLE",  # Esponente M5S (Giuseppe Chiazzese)
+    # Account che sembrano meno direttamente politici o più generici (impostati a None o da ve
+}
